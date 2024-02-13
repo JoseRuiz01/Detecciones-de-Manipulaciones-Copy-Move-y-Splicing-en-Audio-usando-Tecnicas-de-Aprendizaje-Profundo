475b14abdbd6e603ce5a62f1b9fa69c913a90f3e
José Antonio Ruiz Heredia <56451051+JoseRuiz01@users.noreply.github.com>
2024-02-13 16:45:06 +0100

    Refactor Código Audio Spectogramas

--- AudioSpectogramLibrosa.py
+++ AudioSpectogramLibrosa.py
@@ -1,47 +1,10 @@
 import librosa
 import librosa.display
 import numpy as np
-import random
-import os
+from SelectAudios import select_random_audio, search_forgered_audio, select_audios
 import matplotlib.pyplot as plt 
 from scipy.io import wavfile
 from DataPaths import ORIGINAL_DATASET_PATH, COPY_MOVE_DATASET_PATH, SPLICING_DATASET_PATH, SPECTOGRAM_LIBROSA_EXAMPLE_PATH
-
-def select_random_audio(original_path):
-    """
-    Selecciona un audio aleatorio de la carpeta especificada y devuelve su ruta y nombre.
-    """
-    archives = os.listdir(original_path)
-    
-    if not archives:
-        return None
-    
-    random_index = random.randint(0, len(archives)-1)
-    random_audio = archives[random_index]
-    
-    audio_path = os.path.join(original_path, random_audio)
-    audio_name = os.path.splitext(random_audio)[0]
-    
-    return random_audio, audio_name, audio_path
-
-
-def search_forgered_audio(path, audio_name):
-    files_found = []
-    files = os.listdir(path)
-    
-    for file in files:
-        if file.startswith(audio_name) and "_" in file:
-            files_found.append(os.path.join(path, file))
-            
-    return files_found
-
-
-def select_audios(files_found_splicing, files_found_copy_move):
-    random_index = random.randint(0, min(len(files_found_splicing)-1, len(files_found_copy_move)-1))
-    random_audio_splicing = files_found_splicing[random_index]
-    random_audio_cm = files_found_copy_move[random_index]
-    
-    return random_audio_splicing, random_audio_cm
 
 
 def generate_spectogram(original_path, copy_move_path, splicing_path, audio_name):
@@ -49,27 +12,27 @@ def generate_spectogram(original_path, copy_move_path, splicing_path, audio_name
     splicing_audio, _ = librosa.load(splicing_path)
     copy_move_audio, _ = librosa.load(copy_move_path)
     
-    # Ajuste de los parámetros para mejorar la calidad del espectrograma
+    # Change this parameters to improve the quality of the image
     hop_length = 128 # Desplazamiento de la ventana durante la transformación (cuanto menor sea, mayor resolución)
     n_fft = 4096 # antidad de muestras de la señal que se toman para calcular la transformada de Fourier (cuanto mayor sea, mayor resolución)
 
-    # Genera la gráfica del espectrograma
+    # Generate the 3 plots 
     fig, ax = plt.subplots(3, 1, figsize=(8, 6))
     fig.patch.set_facecolor('white')
 
-    # Espectrograma del audio original
+    # Original audio spectogram
     librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(original_audio, hop_length=hop_length, n_fft=n_fft)), ref=np.max), sr=sr, hop_length=hop_length, x_axis='time', y_axis='log', ax=ax[0])
     ax[0].set_title('Espectrograma del audio original')
     ax[0].set_xlabel('Tiempo [s]')
     ax[0].set_ylabel('Amplitud')
 
-    # Espectrograma del audio con copy-move
+    # Copy-Move audio spectogram
     librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(copy_move_audio, hop_length=hop_length, n_fft=n_fft)), ref=np.max), sr=sr, hop_length=hop_length, x_axis='time', y_axis='log', ax=ax[1])
     ax[1].set_title('Espectrograma del audio con copy-move')
     ax[1].set_xlabel('Tiempo [s]')
     ax[1].set_ylabel('Amplitud')
 
-    # Espectrograma del audio con splicing
+    # Splicing audio spectogram
     librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(splicing_audio, hop_length=hop_length, n_fft=n_fft)), ref=np.max), sr=sr, hop_length=hop_length, x_axis='time', y_axis='log', ax=ax[2])
     ax[2].set_title('Espectrograma del audio con splicing')
     ax[2].set_xlabel('Tiempo [s]')
@@ -77,10 +40,13 @@ def generate_spectogram(original_path, copy_move_path, splicing_path, audio_name
 
     plt.tight_layout()
 
-    # Guarda la gráfica como un archivo PNG
+    # Save the image as .png 
     plt.savefig(f'{SPECTOGRAM_LIBROSA_EXAMPLE_PATH}/{audio_name}.png', dpi=2048)
 
 
+
+
+## MAIN ##
 selected_audio_path, audio_name, audio_path = select_random_audio(ORIGINAL_DATASET_PATH)
 
 if selected_audio_path is not None:
--- AudioSpectogramMatplotlib.py
+++ AudioSpectogramMatplotlib.py
@@ -1,110 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
-import os
 from scipy.io import wavfile
 from DataPaths import ORIGINAL_DATASET_PATH, COPY_MOVE_DATASET_PATH, SPLICING_DATASET_PATH, SPECTOGRAM_MATPLOT_EXAMPLE_PATH
+from SelectAudios import select_random_audio, search_forgered_audio, select_audios
 
 
-'''
-We are going to visualize the spectogram of an audio.
-First, we are going to select a .wav file from the OriginalDataset folder, 
-and then we are going to take the same .wav from the other two folders and visualize how it changes, 
-depending on the forgery method applied. 
 
-'''
-
-#seleccionamos un audio aleatorio dentro de la carpeta original
-def select_random_audio(original_path):
-    """
-    Selecciona un audio aleatorio de la carpeta especificada y devuelve su ruta y nombre.
-
-    Args:
-        path_original (str): Ruta a la carpeta que contiene los archivos de audio.
-
-    Returns:
-        tuple: Una tupla con dos elementos:
-            - ruta_audio (str): La ruta completa al archivo de audio seleccionado.
-            - nombre_audio (str): El nombre del archivo de audio sin la extensión.
-
-    None: Si la carpeta está vacía.
-    """
-    archives = os.listdir(original_path)
-    
-    if not archives:
-        return None
-    
-    #Selecciono un indice aleatorio, y lo cojo de la lista
-    random_index = random.randint(0,len(archives)-1)
-    random_audio = archives[random_index]
-    
-    # Construye la ruta completa al archivo de audio
-    audio_path = os.path.join(original_path, random_audio)
-    
-    #nombre sin la extension
-    audio_name = os.path.splitext(random_audio)[0]
-    
-    return random_audio, audio_name, audio_path
-
-
-#busca el audio modificado en las dos carpetas, recibiendo el audio de la carpeta original
-def search_forgered_audio(path, audio_name):
-    files_found = []
-    
-    files = os.listdir(path)
-    
-    for file in files:
-        if file.startswith(audio_name) and "_" in file:
-            files_found.append(os.path.join(path, file))
-            
-    return files_found
-
-    
-def select_audios(files_found_splicing, files_found_copy_move):
-    #Selecciono un indice aleatorio, y lo cojo de la lista
-    
-    random_index = random.randint(0,min(len(files_found_splicing)-1,len(files_found_copy_move)-1))
-    random_audio_splicing = files_found_splicing[random_index]
-    random_audio_cm = files_found_copy_move[random_index]
-    
-    return random_audio_splicing, random_audio_cm
-
-
-def generate_spectogram(original_path, copy_move_path, splicing_path): 
+def generate_spectogram(original_path, copy_move_path, splicing_path, audio_name): 
     fs, original_audio = wavfile.read(original_path)
     fs, splicing_audio = wavfile.read(splicing_path)
     fs, copy_move_audio = wavfile.read(copy_move_path)
     
-    # Encuentra la longitud mínima de los audios
+    # Found the min length of both audios
     min_length = min(len(original_audio), len(splicing_audio), len(copy_move_audio))
 
-    # Recorta los audios a la longitud mínima
+    # Cut the audios with the min length
     original_audio_cut = original_audio[:min_length]
     splicing_audio_cut = splicing_audio[:min_length]
     copy_move_audio_cut = copy_move_audio[:min_length]
     
-    # Crea el tiempo para la gráfica
+    # Create the time line for the plot
     t = np.arange(0, min_length / fs, 1 / fs)
 
-    # Genera la gráfica del espectrograma
+    # Generate the 3 plots
     fig, ax = plt.subplots(3, 1, figsize=(12, 9))  # Crea 3 subplots
     fig.patch.set_facecolor('white')
 
-    # Espectrograma del audio original
+    # Original audio spectogram
     ax[0].plot(t, original_audio_cut)
     ax[0].set_title('Audio original en el dominio del tiempo')
     ax[0].set_xlabel('Tiempo [s]')
     ax[0].set_ylabel('Amplitud')
     ax[0].grid(True)
 
-    # Espectrograma del audio con copy-move
+    # Copy-Move audio spectogram
     ax[1].plot(t, copy_move_audio_cut, c='tab:green')  # Color verde
     ax[1].set_title('Audio con copy-move en el dominio del tiempo')
     ax[1].set_xlabel('Tiempo [s]')
     ax[1].set_ylabel('Amplitud')
     ax[1].grid(True)
 
-    # Espectrograma del audio con splicing
+    # Splicing audio spectogram
     ax[2].plot(t, splicing_audio_cut, c='tab:red')
     ax[2].set_title('Audio con splicing en el dominio del tiempo')
     ax[2].set_xlabel('Tiempo [s]')
@@ -113,12 +49,13 @@ def generate_spectogram(original_path, copy_move_path, splicing_path):
 
     plt.tight_layout()
 
-    # Guarda la gráfica como un archivo PNG
+    # Save the image as .png 
     plt.savefig(f'{SPECTOGRAM_MATPLOT_EXAMPLE_PATH}/{audio_name}.png')
 
     
 
 
+## MAIN ##
 selected_audio_path, audio_name, audio_path = select_random_audio(ORIGINAL_DATASET_PATH)
 
 if selected_audio_path is not None:
@@ -126,8 +63,6 @@ if selected_audio_path is not None:
 else:
     print("La carpeta está vacía.")
     
-
-
 archivos_encontrados_splicing = search_forgered_audio(SPLICING_DATASET_PATH, audio_name)
 files_found_copy_move = search_forgered_audio(COPY_MOVE_DATASET_PATH, audio_name)
 audio1, audio2 = select_audios(archivos_encontrados_splicing,files_found_copy_move)
@@ -135,5 +70,4 @@ audio1, audio2 = select_audios(archivos_encontrados_splicing,files_found_copy_mo
 print(f"Elemento de la lista 1: {audio1}")
 print(f"Elemento de la lista 2: {audio2}")
 
-
-generate_spectogram(audio_path,audio1,audio2)
+generate_spectogram(audio_path, audio1, audio2, audio_name)
